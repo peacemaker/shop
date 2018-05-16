0ea542838fe4d76f947e5985ca8f1ea117718aa6
denys <dsolyanyk@gmail.com>
2018-05-16 11:59:30 +0300

    3. Добавьте несколько простых свойств для компонента ProductComponent и несколько свойств в виде массива. Выведите их в темплейт:     - name: string     - description: string     - price: number     - category: enum (Создайте enum с несколькими категориями) https://www.typescriptlang.org/docs/handbook/enums.html     - isAvailable: boolean     - ...

--- src/app/product-component/product-component.component.ts
+++ src/app/product-component/product-component.component.ts
@@ -1,7 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 
+
 enum Category {
-  Phone, TV, Notebook
+  NA = 'n/a',
+  Phone = 'phone',
+  TV = 'TV',
+  Notebook = 'Notebook'
 }
 
 @Component({
@@ -10,10 +14,10 @@ enum Category {
   styleUrls: ['./product-component.component.css']
 })
 export class ProductComponentComponent implements OnInit {
-  name: string;
-  description: string;
+  name: string = 'Default';
+  description: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
   price: number;
-  category = Category;
+  category: Category = Category.NA;
   isAvailable: boolean;
 
   constructor() {

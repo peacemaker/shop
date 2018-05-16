ab490ad35fecc32482ee718f65af500caa6587ba
denys <dsolyanyk@gmail.com>
2018-05-16 10:34:05 +0300

    Создайте компонент ProductComponent. Используйте его в AppComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductComponentComponent } from './product-component/product-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponentComponent
   ],
   imports: [
     BrowserModule
